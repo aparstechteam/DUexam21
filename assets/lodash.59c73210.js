225e9a4e8ef63f70f4b1bc8d2db5ce4a72488fac
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2022-05-31 22:31:32 +0600

    leaderboard written

--- assets/lodash.59c73210.js
+++ assets/lodash.59c73210.js
@@ -1,4 +1,4 @@
-import{N as jt}from"./index.68fadfc4.js";var Ri={exports:{}};/**
+import{N as jt}from"./index.234665cb.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
